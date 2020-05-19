8d1b5921ef13408792d20fb5b689afc1ba2c70e7
devethan <devohno.ko@gmail.com>
2020-05-19 13:15:21 +0900

    Make request for subscription in general way

---
+++ src/subscriptions/User.ts
@@ -0,0 +1,39 @@
+// General Relay Subscription
+import {
+  GraphQLTaggedNode,
+  Variables,
+  graphql,
+  requestSubscription,
+} from 'react-relay';
+import { RecordSourceSelectorProxy, SelectorData } from 'relay-runtime';
+import environment from '../relay/RelayEnvironment';
+
+const UserSubscription: GraphQLTaggedNode = graphql`
+  subscription UserSubscription {
+    userSignedIn {
+      id
+      email
+    }
+  }
+`;
+
+export default (): void => {
+  const variables: Variables = {};
+  const config = {
+    subscription: UserSubscription,
+    variables,
+    onCompleted: (): void => {
+      console.log('Subscription is now closed.');
+    },
+    onError: (error: Error): void => console.log('An error occured:', error),
+    updater: (store: RecordSourceSelectorProxy, data: SelectorData): void => {
+      const payload = store.getRootField('userSignedIn');
+      const email = payload?.getValue('email');
+      console.log(email);
+    },
+  };
+
+  console.log('Listening...');
+
+  requestSubscription(environment, config);
+};
