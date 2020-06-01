6421aaae4025e5148e1bbf5061da53d6631bb4d8
devethan <devohno.ko@gmail.com>
2020-06-01 15:55:58 +0900

    Update eslint setting for relay

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
+  rules: {
+    'relay/graphql-syntax': 'error',
+    'relay/compat-uses-vars': 'warn',
+    'relay/graphql-naming': 'error',
+    'relay/generated-flow-types': 'warn',
+    'relay/no-future-added-value': 'warn',
+    'relay/unused-fields': 'warn',
+  },
+  plugins: ['relay'],
   extends: '@dooboo/eslint-config',
 };
