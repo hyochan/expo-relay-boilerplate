45954a1d125d13bdd79b58913b1c7c7ce7f1e335
devethan <devohno.ko@gmail.com>
2020-06-30 15:14:29 +0900

    Modify path of schema.graphql

--- relay.config.js
+++ relay.config.js
@@ -2,6 +2,6 @@ module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: './src',
-  schema: './data/schema.graphql',
+  schema: './src/relay/schema.graphql',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
 };
